c59a7493fbf1a69f28015bb57567d49b366c6288
SCool62 <samthecool300@icloud.com>
2026-01-11 12:05:40 -0800

    2026 tagmaps

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Rebuilt";
+  public static final String MAVEN_NAME = "Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "03476351b4b0c0c7d683d9d0515710917301c8c0";
-  public static final String GIT_DATE = "2026-01-11 10:27:23 PST";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "c1fc6bab72e96fdd448e7273995587d29fc17414";
+  public static final String GIT_DATE = "2026-01-11 10:31:08 PST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-11 10:30:24 PST";
-  public static final long BUILD_UNIX_TIME = 1768156224066L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-11 11:55:21 PST";
+  public static final long BUILD_UNIX_TIME = 1768161321995L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
