374677c3a45151c29c65dd2d72a20da5572e77e4
SCool62 <samthecool300@icloud.com>
2026-01-11 14:35:19 -0800

    Button binding

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "4b09cc50be52716d5b15b56dac231b9f36e54afb";
-  public static final String GIT_DATE = "2026-01-11 14:20:03 PST";
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "fc2fe61f1e3958aae0020eb24d19d98b5c582c76";
+  public static final String GIT_DATE = "2026-01-11 14:29:52 PST";
   public static final String GIT_BRANCH = "feature/static-aa";
-  public static final String BUILD_DATE = "2026-01-11 14:26:33 PST";
-  public static final long BUILD_UNIX_TIME = 1768170393426L;
+  public static final String BUILD_DATE = "2026-01-11 14:31:40 PST";
+  public static final long BUILD_UNIX_TIME = 1768170700186L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
