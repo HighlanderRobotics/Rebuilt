c1fc6bab72e96fdd448e7273995587d29fc17414
spellingcat <70864274+spellingcat@users.noreply.github.com>
2026-01-11 10:31:08 -0800

    gitignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "0d6652e92265ec77ada96d964f7d21d9fddb7ebd";
-  public static final String GIT_DATE = "2026-01-10 19:27:17 PST";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "03476351b4b0c0c7d683d9d0515710917301c8c0";
+  public static final String GIT_DATE = "2026-01-11 10:27:23 PST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-11 10:26:29 PST";
-  public static final long BUILD_UNIX_TIME = 1768155989187L;
+  public static final String BUILD_DATE = "2026-01-11 10:30:24 PST";
+  public static final long BUILD_UNIX_TIME = 1768156224066L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
