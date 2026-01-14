1551b6c8bd2435cfdaafac42368d7c4068f9e841
ronansoergel9 <Ronansoergel@gmail.com>
2026-01-13 22:05:46 -0800

    Create RollerIOCTRESim.java

---
+++ src/main/java/frc/robot/components/rollers/RollerIOCTRESim.java
@@ -0,0 +1,38 @@
+package frc.robot.components.rollers;
+
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+import com.ctre.phoenix6.sim.TalonFXSimState.MotorType;
+
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class RollerIOCTRESim extends RollerIOReal {
+
+    private final DCMotorSim motorSim;
+    private TalonFXSimState talonSim;
+    private double lastLoopTime = 0.0;
+
+    public RollerIOCTRESim(int motorID, TalonFXConfiguration config, DCMotorSim motorSim, MotorType motorType) {
+
+        super(motorID, config);
+        this.motorSim = motorSim;
+        talonSim = rollerMotor.getSimState();
+        talonSim.setMotorType(motorType);
+
+        Notifier notifier = new Notifier(() -> {
+            double deltaTime = (Utils.getCurrentTimeSeconds() - lastLoopTime);
+            lastLoopTime = Utils.getCurrentTimeSeconds();
+            talonSim.setSupplyVoltage(RobotController.getBatteryVoltage());
+            motorSim.setInputVoltage(talonSim.getMotorVoltage());
+            motorSim.update(deltaTime);
+            talonSim.setRawRotorPosition(motorSim.getAngularPositionRotations() * motorSim.getGearing());
+            talonSim.setRotorVelocity(motorSim.getAngularVelocityRPM() / 60 * motorSim.getGearing());
+
+        });
+        notifier.startPeriodic(0.002);
+    }
+
+}
