1038c7aa06b6100610e8bd2f45adfa3c54eb6290
radsuperpunch <kai@nagpal.info>
2026-01-14 16:20:55 -0800

    mostly done

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -1,32 +1,44 @@
 package frc.robot.subsystems.intake;
 
-import java.util.function.DoubleSupplier;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.components.rollers.RollerIOInputsAutoLogged;
+import frc.robot.components.rollers.RollerIOReal;
+import org.littletonrobotics.junction.Logger;
 
-public class IntakeSubsystem extends SubsystemBase{
-    
-    public enum IntakeState{
-        private IntakeSubsystem io;
-        private IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
+public class IntakeSubsystem extends SubsystemBase {
 
-        public void IntakeSubsystem(IntakeIOReal io){
-            this.io = io;
+  private RollerIOReal io;
+  private RollerIOInputsAutoLogged inputs = new RollerIOInputsAutoLogged();
 
+  public void IntakeSubsystem(RollerIOReal io) {
+    this.io = io;
+  }
 
-    }
-    }
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Roller", inputs);
+  }
 
+  // TODO get actual values
+  public Command Intake() {
+    return this.run(
+        () -> {
+          io.setRollerVoltage(5);
+        });
+  }
 
-    public Command setVoltage(DoubleSupplier volts) {
-        return this.run(
-            () -> {
+  public Command Outake() {
+    return this.run(
+        () -> {
+          io.setRollerVoltage(-2);
+        });
+  }
 
-                io.setVoltage(volts.getAsDouble());
-            });
-    }
-    public Command getVoltage() {
-        return io.getVoltage();
-    }
+  public Command Rest() {
+    return this.run(
+        () -> {
+          io.setRollerVoltage(0);
+        });
+  }
 }
