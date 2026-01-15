d7bdf1040fbc89760f5049350064211b46c82307
Dhilan Belur <Dhilan.belur@gmail.com>
2026-01-14 23:45:37 -0800

    Edits to Indexer Subsystem; Mostly building out the constructor and periodic method.

--- src/main/java/frc/robot/subsystems/indexer/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerSubsystem.java
@@ -10,70 +10,68 @@ import frc.robot.components.rollers.RollerIOInputsAutoLogged;
 import frc.robot.components.rollers.RollerIOReal;
 import java.util.function.DoubleSupplier;
 
+import org.littletonrobotics.junction.Logger;
+
 public class IndexerSubsystem extends SubsystemBase {
 
   // Add actual CanBus
 
-  CANBus CANBus = new CANBus();
-  CANrangeIOReal firstCANRange = new CANrangeIOReal(0, CANBus);
-  CANrangeIOReal secondCANRange = new CANrangeIOReal(1, CANBus);
-  CANrangeIOInputsAutoLogged CANRangeInputs = new CANrangeIOInputsAutoLogged();
-  TalonFXConfiguration configs = new TalonFXConfiguration();
-  RollerIOReal rollers = new RollerIOReal(1, configs);
+    CANrangeIOReal firstCANRange;
+    CANrangeIOReal secondCANRange;
+    RollerIOReal rollerIO;
+    CANrangeIOInputsAutoLogged firstCANRangeInputs = new CANrangeIOInputsAutoLogged();
+    CANrangeIOInputsAutoLogged secondCANRangeInputs = new CANrangeIOInputsAutoLogged();
+    RollerIOInputsAutoLogged rollerInputs = new RollerIOInputsAutoLogged();
 
-  RollerIOInputsAutoLogged rollerInputs = new RollerIOInputsAutoLogged();
+  public IndexerSubsystem(CANBus canbus, RollerIOReal roller) {
+    firstCANRange = new CANrangeIOReal(0, canbus);
+    secondCANRange = new CANrangeIOReal(1, canbus);
+    rollerIO = roller;
 
-  public IndexerSubsystem() {}
-
-  public boolean isFull(boolean firstBeamBreak, boolean secondBeamBreak) {
-    if (firstBeamBreak && secondBeamBreak) {
-      return true;
-    } else {
-      return false;
-    }
   }
 
-  public boolean isEmpty(boolean firstBeamBreak, boolean secondBeamBreak) {
-    if (!firstBeamBreak && !secondBeamBreak) {
-      return true;
-    } else {
-      return false;
-    }
+  public boolean isFull() {
+    
+    return (firstCANRangeInputs.isDetected && secondCANRangeInputs.isDetected);
+
   }
 
-  public boolean isPartiallyFull(boolean firstBeamBreak, boolean secondBeamBreak) {
-    if (!firstBeamBreak && secondBeamBreak) {
-      return true;
-    } else {
-      return false;
-    }
+  public boolean isEmpty() {
+    return (!firstCANRangeInputs.isDetected && !secondCANRangeInputs.isDetected);
   }
 
-  public Command index(DoubleSupplier volts) {
+  public boolean isPartiallyFull() {
+    return (!firstCANRangeInputs.isDetected && secondCANRangeInputs.isDetected);
+  }
+
+  public Command index() {
     return this.run(
         () -> {
-          rollers.setRollerVoltage(volts.getAsDouble());
+          rollerIO.setRollerVoltage(5);
         });
   }
 
-  public Command score(DoubleSupplier volts) {
+  public Command score() {
     return this.run(
         () -> {
-          rollers.setRollerVoltage(volts.getAsDouble());
+          rollerIO.setRollerVoltage(10);
         });
   }
 
-  public Command outtake(DoubleSupplier volts) {
+  public Command outtake() {
     return this.run(
         () -> {
-          rollers.setRollerVoltage(volts.getAsDouble());
+          rollerIO.setRollerVoltage(-5);
         });
   }
 
   @Override
   public void periodic() {
-    firstCANRange.updateInputs(CANRangeInputs);
-    secondCANRange.updateInputs(CANRangeInputs);
-    rollers.updateInputs(rollerInputs);
+    firstCANRange.updateInputs(firstCANRangeInputs);
+    Logger.processInputs("Indexer/First Beambreak", firstCANRangeInputs);
+    secondCANRange.updateInputs(secondCANRangeInputs);
+    Logger.processInputs("Indexer/Second Beambreak", secondCANRangeInputs);
+    rollerIO.updateInputs(rollerInputs);
+    Logger.processInputs("Indexer/Roller", rollerInputs);
   }
 }
