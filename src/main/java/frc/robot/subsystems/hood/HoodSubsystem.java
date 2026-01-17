4e1111fbf9e90d05d43638a97f05b6b1f2387a9e
SCool62 <samthecool300@icloud.com>
2026-01-16 17:02:06 -0800

    Hood sysid

--- src/main/java/frc/robot/subsystems/hood/HoodSubsystem.java
+++ src/main/java/frc/robot/subsystems/hood/HoodSubsystem.java
@@ -7,14 +7,28 @@ package frc.robot.subsystems.hood;
 import com.google.common.base.Supplier;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Mechanism;
+
+import static edu.wpi.first.units.Units.Volts;
+
 import org.littletonrobotics.junction.Logger;
 
 public class HoodSubsystem extends SubsystemBase {
   private HoodIO hoodIO;
   private HoodIOInputsAutoLogged hoodInputs = new HoodIOInputsAutoLogged();
 
+  private SysIdRoutine hoodSysid = new SysIdRoutine(new Config(), new Mechanism((voltage) -> hoodIO.setHoodVoltage(voltage.in(Volts)), null, this));
+
   public static double GEAR_RATIO = 147.0 / 13.0;
+
+  public static Rotation2d MAX_ROTATION = Rotation2d.fromDegrees(90); // TODO: ACTUAL VALUE
+  public static Rotation2d MIN_ROTATION = Rotation2d.fromDegrees(90); // TODO: ACTUAL VALUE
+
 
   /** Creates a new HoodSubsystem. */
   public HoodSubsystem(HoodIO io) {
@@ -29,5 +43,14 @@ public class HoodSubsystem extends SubsystemBase {
   public void periodic() {
     hoodIO.updateInputs(hoodInputs);
     Logger.processInputs("Shooter/Hood", hoodInputs);
+  }
+
+  public Command runHoodSysid() {
+    return Commands.sequence(
+      hoodSysid.quasistatic(Direction.kForward).until(() -> hoodInputs.hoodPositionRotations.getDegrees() > (MAX_ROTATION.getDegrees() - 5)), // Stop before endstop
+      hoodSysid.quasistatic(Direction.kReverse).until(() -> hoodInputs.hoodPositionRotations.getDegrees() > (MIN_ROTATION.getDegrees() + 5)),
+      hoodSysid.dynamic(Direction.kForward).until(() -> hoodInputs.hoodPositionRotations.getDegrees() > (MAX_ROTATION.getDegrees() - 5)),
+      hoodSysid.dynamic(Direction.kReverse).until(() -> hoodInputs.hoodPositionRotations.getDegrees() > (MIN_ROTATION.getDegrees() + 5))
+    );
   }
 }
