63e589220ca922bf4f41765cd71923088e1442c6
spellingcat <70864274+spellingcat@users.noreply.github.com>
2026-01-27 16:49:18 -0800

    fix br

--- src/main/java/frc/robot/subsystems/swerve/constants/AlphaSwerveConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/constants/AlphaSwerveConstants.java
@@ -94,12 +94,12 @@ public class AlphaSwerveConstants extends SwerveConstants {
             new Transform3d(
                 new Translation3d(
                     Units.inchesToMeters(-9.859),
-                    Units.inchesToMeters(9.665),
+                    Units.inchesToMeters(-9.665),
                     Units.inchesToMeters(8.844)),
                 new Rotation3d(
                     Units.degreesToRadians(0.0),
                     Units.degreesToRadians(-90 + 61.875), // -61.875 - 45 + 90),
-                    Units.degreesToRadians(-63.835 + 180))),
+                    Units.degreesToRadians(-63.835 - 90))),
             BACK_RIGHT_CAMERA_MATRIX,
             BACK_RIGHT_DIST_COEFFS);
     final CameraConstants backLeftCamConstants =
