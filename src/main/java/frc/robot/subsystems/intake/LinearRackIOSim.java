35529d85fadf74a93d038c036670d5209a823e39
SCool62 <samthecool300@icloud.com>
2026-01-29 17:53:35 -0800

    Spotless

--- src/main/java/frc/robot/subsystems/intake/LinearRackIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/LinearRackIOSim.java
@@ -6,7 +6,6 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.sim.ChassisReference;
 import com.ctre.phoenix6.sim.TalonFXSimState;
 import com.ctre.phoenix6.sim.TalonFXSimState.MotorType;
-
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.math.util.Units;
@@ -18,7 +17,16 @@ public class LinearRackIOSim extends LinearRackIO {
   // TODO: SHOULD THIS BE AN ELEVATOR?
   ElevatorSim physicsSim =
       new ElevatorSim(
-          LinearSystemId.createElevatorSystem(DCMotor.getKrakenX44Foc(1), Units.lbsToKilograms(10.0), LintakeSubsystem.RACK_PINION_DIAMETER_METERS / 2, LintakeSubsystem.RACK_GEAR_RATIO), DCMotor.getKrakenX44Foc(1), 0.0, LintakeSubsystem.MAX_EXTENSION_METERS, false, 0.0);
+          LinearSystemId.createElevatorSystem(
+              DCMotor.getKrakenX44Foc(1),
+              Units.lbsToKilograms(10.0),
+              LintakeSubsystem.RACK_PINION_DIAMETER_METERS / 2,
+              LintakeSubsystem.RACK_GEAR_RATIO),
+          DCMotor.getKrakenX44Foc(1),
+          0.0,
+          LintakeSubsystem.MAX_EXTENSION_METERS,
+          false,
+          0.0);
 
   private static final double SIM_LOOP_PERIOD = 0.002; // 2 ms
   private Notifier notifier;
@@ -46,9 +54,13 @@ public class LinearRackIOSim extends LinearRackIO {
 
               // I think these should be multiplied?
               talonSim.setRawRotorPosition(
-                  physicsSim.getPositionMeters() * (LintakeSubsystem.RACK_GEAR_RATIO * (Math.PI * LintakeSubsystem.RACK_PINION_DIAMETER_METERS)));
+                  physicsSim.getPositionMeters()
+                      * (LintakeSubsystem.RACK_GEAR_RATIO
+                          * (Math.PI * LintakeSubsystem.RACK_PINION_DIAMETER_METERS)));
               talonSim.setRotorVelocity(
-                  physicsSim.getVelocityMetersPerSecond() * (LintakeSubsystem.RACK_GEAR_RATIO * (Math.PI * LintakeSubsystem.RACK_PINION_DIAMETER_METERS)));
+                  physicsSim.getVelocityMetersPerSecond()
+                      * (LintakeSubsystem.RACK_GEAR_RATIO
+                          * (Math.PI * LintakeSubsystem.RACK_PINION_DIAMETER_METERS)));
             });
 
     notifier.startPeriodic(SIM_LOOP_PERIOD);
