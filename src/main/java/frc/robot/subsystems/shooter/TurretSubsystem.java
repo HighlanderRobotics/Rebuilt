7b43f6c918d615f3be8e8e8582a66cd241cb1a8a
lQuasar9206 <jtyszee@gmail.com>
2026-02-01 20:11:06 -0800

    real gear ratios

--- src/main/java/frc/robot/subsystems/shooter/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/TurretSubsystem.java
@@ -19,13 +19,13 @@ import org.littletonrobotics.junction.Logger;
 /** Pivoting hooded shooter (turret). !! COMP !! */
 public class TurretSubsystem extends SubsystemBase implements Shooter {
   /** Creates a new TurretSubsystem. */
-  public static double HOOD_GEAR_RATIO_C = 1;
-  public static double FLYWHEEL_GEAR_RATIO_C = 1;
+  public static double HOOD_GEAR_RATIO_C = 58.96875;
+  public static double FLYWHEEL_GEAR_RATIO_C = 0.84615384615;
 
   public static Rotation2d HOOD_MAX_ROTATION = Rotation2d.fromDegrees(40);
   public static Rotation2d HOOD_MIN_ROTATION = Rotation2d.fromDegrees(2);
 
-  public static double FLYWHEEL_GEAR_RATIO = 28.0 / 24.0;
+  
 
   public static double FLYWHEEL_VELOCITY_TOLERANCE_ROTATIONS_PER_SECOND = 5.0;
 
