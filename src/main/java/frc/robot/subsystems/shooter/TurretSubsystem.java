02a7f5dc013d3daea12897f99ff7a4acc6d154b1
vivi-o <vivie@oesterer.com>
2026-02-10 18:56:36 -0800

    idk

--- src/main/java/frc/robot/subsystems/shooter/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/TurretSubsystem.java
@@ -47,7 +47,6 @@ public class TurretSubsystem extends SubsystemBase implements Shooter {
   // shouldnt this be private final
   HoodIO hoodIO;
   HoodIOInputsAutoLogged hoodInputs = new HoodIOInputsAutoLogged();
-
   FlywheelIO flywheelIO;
   FlywheelIOInputsAutoLogged flywheelInputs = new FlywheelIOInputsAutoLogged();
 
