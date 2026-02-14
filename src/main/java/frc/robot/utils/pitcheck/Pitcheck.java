1ccbe9dc4e56b3f6d101b477c74782e891369c15
lQuasar9206 <jtyszee@gmail.com>
2026-02-13 16:14:29 -0800

    updated branch

--- src/main/java/frc/robot/utils/pitcheck/Pitcheck.java
+++ src/main/java/frc/robot/utils/pitcheck/Pitcheck.java
@@ -8,8 +8,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import frc.robot.subsystems.intake.LintakeSubsystem;
 import frc.robot.subsystems.indexer.SpindexerSubsystem;
+import frc.robot.subsystems.intake.LintakeSubsystem;
 import java.util.function.BooleanSupplier;
 
 /** Add your docs here. */
@@ -19,20 +19,20 @@ public class Pitcheck {
   SpindexerSubsystem spindexer = new SpindexerSubsystem();
   BooleanSupplier intakeRunningForward =
       () -> intake.getRollerVoltage() > 9.0 && intake.getRollerVoltage() < 11.0;
-  BooleanSupplier intakeRunningBackward = 
-        ()->intake.getRollerVoltage()<-9.0 && intake.getRollerVoltage()>-11.0;
-  BooleanSupplier intakeRest = 
-        ()-> intake.getRollerVoltage()==0;
-
-
+  BooleanSupplier intakeRunningBackward =
+      () -> intake.getRollerVoltage() < -9.0 && intake.getRollerVoltage() > -11.0;
+  BooleanSupplier intakeRest = () -> intake.getRollerVoltage() == 0;
 
   public void pitcheck(Subsystem subsystem, Command command, BooleanSupplier endState) {
-    SmartDashboard.putData("intakeRoller", new SequentialCommandGroup(
-    pitCheck(intake, intake.intake(), intakeRunningForward),
-    pitCheck(intake, intake.outtake(), intakeRunningBackward),
-    pitCheck(intake, intake.rest(), intakeRest)));
-    SmartDashboard.putData("spindexer", new SequentialCommandGroup(
-        pitCheck(spindexer, spindexer.kick(),spindexerKick)));
+    SmartDashboard.putData(
+        "intakeRoller",
+        new SequentialCommandGroup(
+            pitCheck(intake, intake.intake(), intakeRunningForward),
+            pitCheck(intake, intake.outtake(), intakeRunningBackward),
+            pitCheck(intake, intake.rest(), intakeRest)));
+    SmartDashboard.putData(
+        "spindexer",
+        new SequentialCommandGroup(pitCheck(spindexer, spindexer.kick(), spindexerKick)));
   }
 
   private Command pitCheck(Subsystem subsystem, Command command, BooleanSupplier endstate) {
