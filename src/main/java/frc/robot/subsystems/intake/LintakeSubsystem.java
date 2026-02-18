1f629e5f2153641725e88296a5522a73557eb434
spellingcat <70864274+spellingcat@users.noreply.github.com>
2026-02-17 20:44:34 -0800

    decrease intake current limit

--- src/main/java/frc/robot/subsystems/intake/LintakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/LintakeSubsystem.java
@@ -218,7 +218,7 @@ public class LintakeSubsystem extends SubsystemBase implements Intake {
     config.Slot0.kD = 0.0;
 
     // TODO: TUNE
-    config.CurrentLimits.StatorCurrentLimit = 80.0;
+    config.CurrentLimits.StatorCurrentLimit = 60.0;
     config.CurrentLimits.StatorCurrentLimitEnable = true;
     config.CurrentLimits.SupplyCurrentLimit = 40.0;
     config.CurrentLimits.SupplyCurrentLimitEnable = true;
