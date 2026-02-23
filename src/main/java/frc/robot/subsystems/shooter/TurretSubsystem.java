0c13a7d5dcb8fa7413786db4dd2c89e5b3a7db27
SCool62 <samthecool300@icloud.com>
2026-02-22 18:06:01 -0800

    Clarify in comment

--- src/main/java/frc/robot/subsystems/shooter/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/TurretSubsystem.java
@@ -53,7 +53,7 @@ public class TurretSubsystem extends SubsystemBase implements Shooter {
   // TODO: REDO THIS HARDSTOP WHEN FIXED??
   public static Rotation2d TURRET_REAR_HARDSTOP_ANGLE =
       // Changed to avoid cooking cable chain/wires
-      // Plus 0 because then the rotation2d automatically wraps the value between -0.5 and 0.5
+      // Plus 0 because then the rotation2d automatically wraps the value between -0.5 and 0.5 (worked in sim)
       Rotation2d.fromRotations(-0.736084).plus(Rotation2d.kZero); // 0.25 // -0.75 // -0.719536);
   public static Rotation2d TURRET_FORWARD_HARDSTOP_ANGLE =
       Rotation2d.fromRotations(-0.014404); // -0.0354 // 0.011378);
