476104401f88258ebf45fb3a59820ed5092856cc
spellingcat <70864274+spellingcat@users.noreply.github.com>
2026-02-25 20:58:04 -0800

    reverse recalibrating gyro?????

--- src/main/java/frc/robot/subsystems/swerve/constants/comp/R1WispSwerveConstants.java
+++ src/main/java/frc/robot/subsystems/swerve/constants/comp/R1WispSwerveConstants.java
@@ -201,7 +201,7 @@ public class R1WispSwerveConstants extends SwerveConstants {
     Pigeon2Configuration config = new Pigeon2Configuration();
     config.MountPose.MountPosePitch = -0.20375922322273254; // -0.420589417219162;
     config.MountPose.MountPoseRoll = -179.13818359375; // -179.8539581298828;
-    config.MountPose.MountPoseYaw = 90.93168640136719; // -86.66709899902344;
+    config.MountPose.MountPoseYaw = -90.93168640136719; // -86.66709899902344;
     return config;
   }
 
