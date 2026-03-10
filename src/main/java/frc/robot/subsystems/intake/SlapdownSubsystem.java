a3075d67390947ec3c0f5465b801fa5ba26d5184
SCool62 <samthecool300@icloud.com>
2026-03-09 21:56:30 -0700

    Current Limit

--- src/main/java/frc/robot/subsystems/intake/SlapdownSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/SlapdownSubsystem.java
@@ -254,9 +254,8 @@ public class SlapdownSubsystem extends SubsystemBase implements Intake {
     config.Slot0.kP = 8.0;
     config.Slot0.kD = 0.3;
 
-    // TODO: TUNE
-    config.CurrentLimits.StatorCurrentLimit = 80.0; // glup
-    config.CurrentLimits.StatorCurrentLimitEnable = false;
+    config.CurrentLimits.StatorCurrentLimit = 45.0; // glup
+    config.CurrentLimits.StatorCurrentLimitEnable = true;
     config.CurrentLimits.SupplyCurrentLimit = 40.0;
     config.CurrentLimits.SupplyCurrentLimitEnable = true;
 
