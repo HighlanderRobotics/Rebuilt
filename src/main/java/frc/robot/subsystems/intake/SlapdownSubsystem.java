8fe76a649ce3a90ddf806ecb82753871fac18130
spellingcat <70864274+spellingcat@users.noreply.github.com>
2026-03-13 14:18:28 -0700

    WE ARE SO BACK

--- src/main/java/frc/robot/subsystems/intake/SlapdownSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/SlapdownSubsystem.java
@@ -210,8 +210,8 @@ public class SlapdownSubsystem extends SubsystemBase implements Intake {
     config.CurrentLimits.SupplyCurrentLimitEnable = true;
 
     // TODO: TUNE
-    config.MotionMagic.MotionMagicCruiseVelocity = 1;
-    config.MotionMagic.MotionMagicAcceleration = 1;
+    config.MotionMagic.MotionMagicCruiseVelocity = 5;
+    config.MotionMagic.MotionMagicAcceleration = 5;
 
     return config;
   }
