fd3d3d79ff74870b8b51e3ef9a8bd33ca532a4bc
spellingcat <70864274+spellingcat@users.noreply.github.com>
2026-03-13 08:30:48 -0700

    fix min position

--- src/main/java/frc/robot/subsystems/intake/SlapdownSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/SlapdownSubsystem.java
@@ -31,7 +31,7 @@ import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
 public class SlapdownSubsystem extends SubsystemBase implements Intake {
-  public static final Rotation2d PIVOT_MIN_POSITION = Rotation2d.fromRotations(-0.049805);
+  public static final Rotation2d PIVOT_MIN_POSITION = Rotation2d.fromRotations(-0.050781);
   public static final Rotation2d PIVOT_MAX_POSITION =
       Rotation2d.fromDegrees(122); // Not so sure abt this one...
   public static final Rotation2d PIVOT_EXTENDED_POSITION = PIVOT_MIN_POSITION;
