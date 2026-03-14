1924eda0baf737be5241ed11053235103b2de3b8
spellingcat <70864274+spellingcat@users.noreply.github.com>
2026-03-14 13:45:55 -0700

    Update at 'Sat Mar 14 13:45:55 PDT 2026'

--- src/main/java/frc/robot/subsystems/intake/SlapdownSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/SlapdownSubsystem.java
@@ -94,14 +94,14 @@ public class SlapdownSubsystem extends SubsystemBase implements Intake {
             this.run(
                     () -> {
                       pivotIO.setMotorPositionSetpoint(PIVOT_EXTENDED_POSITION);
-                      rollerIO.setRollerVelocity(50.0);
+                      // rollerIO.setRollerVelocity(50.0);
                     })
                 .until(atExtensionTrigger),
             this.run(
                     () -> {
                       pivotIO.setMotorPositionSetpoint(
                           PIVOT_EXTENDED_POSITION.plus(Rotation2d.fromDegrees(20)));
-                      rollerIO.setRollerVelocity(50.0);
+                      // rollerIO.setRollerVelocity(50.0);
                     })
                 .until(atExtensionTrigger))
         .repeatedly();
