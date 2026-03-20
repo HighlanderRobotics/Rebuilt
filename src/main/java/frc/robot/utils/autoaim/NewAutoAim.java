3eed5e7f733f96451794d855d7c71ac71721fff4
spellingcat <70864274+spellingcat@users.noreply.github.com>
2026-03-20 00:11:14 -0700

    hack together mech a + newton's method i dont know please dont ask me to explain this

---
+++ src/main/java/frc/robot/utils/autoaim/NewAutoAim.java
@@ -0,0 +1,153 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.utils.autoaim;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Twist2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import frc.robot.subsystems.shooter.TurretSubsystem;
+import frc.robot.utils.autoaim.InterpolatingShotTree.ShotData;
+import org.littletonrobotics.junction.Logger;
+
+/** Add your docs here. */
+public class NewAutoAim {
+  private static boolean outOfRange = false; // TODO not sure if this should be true by default
+
+  public record ShotParams(ShotData shotData, Rotation2d turretAngle) {}
+
+  public ShotParams calculate(Pose2d robotPose, ChassisSpeeds fieldRelativeSpeeds, Translation2d target, InterpolatingShotTree tree) {
+    ChassisSpeeds robotRelativeSpeeds =
+        ChassisSpeeds.fromFieldRelativeSpeeds(fieldRelativeSpeeds, robotPose.getRotation());
+    // this is robot relative speeds because "The twist is a change in pose in the robot's
+    // coordinate frame"
+    Pose2d compensatedPose =
+        robotPose.exp(
+            new Twist2d(
+                robotRelativeSpeeds.vxMetersPerSecond * (AutoAim.LATENCY_COMPENSATION_SECS),
+                robotRelativeSpeeds.vyMetersPerSecond * (AutoAim.LATENCY_COMPENSATION_SECS),
+                robotRelativeSpeeds.omegaRadiansPerSecond * (AutoAim.LATENCY_COMPENSATION_SECS)));
+    Pose2d turretPose = AutoAim.getTurretPose(compensatedPose);
+    double distanceToTarget = robotPose.getTranslation().getDistance(target);
+
+    // the turret isn't in the center of the robot, so if the robot is rotating the turret's x and y
+    // are changing slightly
+    double turretFieldRelativeVelocityX =
+        fieldRelativeSpeeds.vxMetersPerSecond
+            + fieldRelativeSpeeds.omegaRadiansPerSecond
+                * (TurretSubsystem.ROBOT_TO_TURRET_TRANSLATION.getY()
+                        * Math.cos(compensatedPose.getRotation().getRadians())
+                    - TurretSubsystem.ROBOT_TO_TURRET_TRANSLATION.getX()
+                        * Math.sin(compensatedPose.getRotation().getRadians()));
+    double turretFieldRelativeVelocityY =
+        fieldRelativeSpeeds.vyMetersPerSecond
+            + fieldRelativeSpeeds.omegaRadiansPerSecond
+                * (TurretSubsystem.ROBOT_TO_TURRET_TRANSLATION.getX()
+                        * Math.cos(compensatedPose.getRotation().getRadians())
+                    - TurretSubsystem.ROBOT_TO_TURRET_TRANSLATION.getY()
+                        * Math.sin(compensatedPose.getRotation().getRadians()));
+
+    Pose2d lookaheadPose = turretPose;
+
+    // for (int i = 0; i < 20; i++) {
+    //   double tof = tree.get(distanceToTarget).timeOfFlightSecs();
+    //   lookaheadPose =
+    //       turretPose.transformBy(
+    //           new Transform2d(
+    //               new Translation2d(turretFieldRelativeVelocityX * tof, turretFieldRelativeVelocityY * tof),
+    //               Rotation2d.kZero));
+    //   distanceToTarget = lookaheadPose.getTranslation().getDistance(target);
+    // }
+    double tof = tree.get(distanceToTarget).timeOfFlightSecs();
+
+        ShotData baseline = tree.calculateShot(robotPose, target);
+
+    Translation2d turretToTarget = target.minus(turretPose.getTranslation());
+
+    double distance = turretToTarget.getNorm();
+
+    // get just direction of vector because its vector divded by length
+    // dont want to account for magnitude bc the speed we are going and shot tree do
+    // and we just want direction to find dot product
+    Translation2d shotDirection = turretToTarget.div(distance);
+
+    // dot product! <3
+    // get how fast we are going towards where we are shooting
+    // vectors of robot times direction
+    // positive if going towrds
+    // zero is moving perpedicular
+    // negative is going away
+    double turretVelocityAlongShot =
+        turretFieldRelativeVelocityX * shotDirection.getX()
+            + turretFieldRelativeVelocityY * shotDirection.getY();
+
+    // required velocity is like velocity the ball must have so it hits the target while the robot
+    // moving
+    // because the ball velocity is robot velocity + ball velocity
+    // so velocity ball needs to go is our distance / tof - the dot product or velocity along that
+    // shot to account for the robots velocity along the shot
+    double requiredVelocity = (distance / baseline.timeOfFlightSecs()) - turretVelocityAlongShot;
+
+    //we be newtoning
+    for (int i = 0; i < 20; i++) {
+      final double EPSILON = 0.001;
+      // get deriv of velocity (dis/time)
+      double lowVel =
+          (distanceToTarget - EPSILON) / tree.get(distanceToTarget - EPSILON).timeOfFlightSecs();
+      double highVel =
+          (distanceToTarget + EPSILON) / tree.get(distanceToTarget + EPSILON).timeOfFlightSecs();
+      double velDeriv = (highVel - lowVel) / (EPSILON * 2);
+      // newtons method: xn+1 = xn - f(xn)/deriv(xn)
+      // so estimate for new dist is difference between current velocity required velocity over the
+      // deriv
+      // this makes sense because if current vel is larger it will lower current distance to account
+      // for that and if requird is larger it will increase to account for that
+      distanceToTarget -= (distanceToTarget / tof - requiredVelocity) / velDeriv;
+      // update
+      tof = tree.get(distanceToTarget).timeOfFlightSecs();
+    }
+
+    //TODO if we're behind the hub just don't shoot
+
+    Rotation2d turretTarget =
+        target.minus(lookaheadPose.getTranslation()).getAngle();
+    return new ShotParams(tree.get(distanceToTarget), getTurretTargetRotation(turretTarget, robotPose));
+  }
+
+  public static Rotation2d getTurretTargetRotation(
+      Rotation2d turretTargetRotation, Pose2d robotPose) {
+
+    // subtract that from rotation to point at target
+    turretTargetRotation = turretTargetRotation.minus(robotPose.getRotation());
+    Logger.recordOutput("Turret/Unclamped target", turretTargetRotation);
+    // -5 is some insane fudge factor i forgot where it's from
+    double turretTargetDegrees = turretTargetRotation.getDegrees() - 5;
+    // If its in the deadzone, clamp to nearest hardstop
+    outOfRange =
+        turretTargetDegrees > TurretSubsystem.TURRET_FORWARD_HARDSTOP_ANGLE.getDegrees()
+            && (turretTargetDegrees < TurretSubsystem.TURRET_LEFT_HARDSTOP_ANGLE.getDegrees());
+    if (outOfRange) {
+      turretTargetDegrees =
+          // If the requested angle is greater than the halfway point in the deadzone, go to the
+          // left hardstop, otherwise go to forward hardstop
+          turretTargetDegrees
+                  > (TurretSubsystem.TURRET_FORWARD_HARDSTOP_ANGLE.getDegrees()
+                          + TurretSubsystem.TURRET_LEFT_HARDSTOP_ANGLE.getDegrees())
+                      / 2
+              ? TurretSubsystem.TURRET_LEFT_HARDSTOP_ANGLE.getDegrees() + 2
+              : TurretSubsystem.TURRET_FORWARD_HARDSTOP_ANGLE.getDegrees() - 2;
+    }
+
+    Logger.recordOutput("Turret/Clamped target", Rotation2d.fromDegrees(turretTargetDegrees));
+    // Now we need to rewrap this angle to always be negative, with 0 as the forward hardstop
+    turretTargetDegrees = MathUtil.inputModulus(turretTargetDegrees, -360, 0);
+    Logger.recordOutput("Turret/Wrapped target", Rotation2d.fromDegrees(turretTargetDegrees));
+    // ship it
+    return Rotation2d.fromDegrees(turretTargetDegrees);
+  }
+}
