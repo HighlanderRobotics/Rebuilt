f48ed6abc201b5ddeb1a7192ac8d79e0ada616b7
SCool62 <samthecool300@icloud.com>
2026-04-01 13:29:44 -0700

    Doc comments

--- src/main/java/frc/robot/subsystems/swerve/module/Module.java
+++ src/main/java/frc/robot/subsystems/swerve/module/Module.java
@@ -62,6 +62,7 @@ public class Module {
     io.setTurnVoltage(volts);
   }
 
+  /** Stops the module */
   public void stop() {
     io.setDriveVoltage(0);
   }
@@ -76,6 +77,13 @@ public class Module {
     return runClosedLoop(state, 0.0, 0.0);
   }
 
+  /**
+   * Runs closed loop to the specified state, with a force feedforward. Used for automated actions, like auto.
+   * @param state setpoint state
+   * @param forceXNewtons the x-component of the requested force vector
+   * @param forceYNewtons the y-component of the requested force vector
+   * @return the optimized state
+   */
   public SwerveModuleState runClosedLoop(
       SwerveModuleState state, double forceXNewtons, double forceYNewtons) {
     state.optimize(getAngle());
@@ -83,8 +91,8 @@ public class Module {
     // Force on the motor is the total force vector projected onto the velocity vector
     // to project a onto b take ||a||*cos(theta) where theta is the angle between the two vectors
     // We want the magnitude of the projection, so we can ignore the direction of this later
-    final var theta = Math.atan2(forceYNewtons, forceXNewtons) - inputs.turnPosition.getRadians();
-    double forceNewtons = Math.hypot(forceXNewtons, forceYNewtons) * Math.cos(theta);
+    final double thetaRads = Math.atan2(forceYNewtons, forceXNewtons) - inputs.turnPosition.getRadians();
+    double forceNewtons = Math.hypot(forceXNewtons, forceYNewtons) * Math.cos(thetaRads);
     if (Math.signum(forceNewtons) * Math.signum(state.speedMetersPerSecond) < 0) {
       forceNewtons = 0;
     }
