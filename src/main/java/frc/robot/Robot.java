fd759353d8392bccfe09d6e9ab8c86a4b0b1f6df
lQuasar9206 <jtyszee@gmail.com>
2026-04-03 21:22:56 -0700

    renamed

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -805,7 +805,7 @@ public class Robot extends LoggedRobot {
     autoChooser.addOption("Right Bump Outpost Center", autos.getRightBumpOutpostCenterAuto());
     autoChooser.addOption("Right Trench Double Dip Auto", autos.getDoubleDipRightTrench());
     autoChooser.addOption(
-        "RIght Trench DoubleD Disrupt Auto", autos.getDisruptOutpostRightTrench());
+        "Right Trench Disrupt Outpost Auto", autos.getDisruptOutpostRightTrench());
     autoChooser.addOption("Left Neutral Score Twice", autos.getLeftNeutralScoreTwice());
     // autoChooser.addOption("Left Neutral Outpost Score", autos.getLeftNeutralOutpostScore());
     autoChooser.addOption("Hub Depot Outpost", autos.getHubDepotOutpostAuto());
