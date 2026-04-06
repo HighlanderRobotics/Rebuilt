499040f3c19fe53593bbd37921c76c87e86554cf
spellingcat <70864274+spellingcat@users.noreply.github.com>
2026-04-06 12:16:41 -0700

    jarvis more

--- src/main/java/frc/robot/subsystems/indexer/SpindexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/indexer/SpindexerSubsystem.java
@@ -117,9 +117,8 @@ public class SpindexerSubsystem extends SubsystemBase implements Indexer {
                   flywheelRPS.getAsDouble() * Math.PI * TurretSubsystem.FLYWHEEL_DIAMETER_INCHES;
               double x44Speed = flywheelLinearSpeed * 1 / (Math.PI * X44_KICKER_DIAMETER_INCHES);
 
-              double x60Speed = flywheelLinearSpeed * 0.9 / (Math.PI * X60_KICKER_DIAMETER_INCHES);
-              double spinnerSpeed =
-                  flywheelLinearSpeed * 0.85 / (Math.PI * SPINNER_DIAMETER_INCHES);
+              double x60Speed = flywheelLinearSpeed * 0.95 / (Math.PI * X60_KICKER_DIAMETER_INCHES);
+              double spinnerSpeed = flywheelLinearSpeed * 0.9 / (Math.PI * SPINNER_DIAMETER_INCHES);
               spinnerIO.setRollerVelocity(spinnerSpeed);
               x44KickerIO.setRollerVelocity(x44Speed);
               x60KickerIO.setRollerVelocity(x60Speed);
