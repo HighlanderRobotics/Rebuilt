8b566e5b35e75660faf0bfb57946750c69d25a0b
spellingcat <70864274+spellingcat@users.noreply.github.com>
2026-04-10 17:24:16 -0700

    Update at 'Fri Apr 10 17:24:15 PDT 2026'

--- src/main/java/frc/robot/components/camera/Camera.java
+++ src/main/java/frc/robot/components/camera/Camera.java
@@ -142,8 +142,11 @@ public class Camera {
       return infiniteDevs;
     }
 
-    //reject if pose thinks we're out of the field
-    if (estimation.estimatedPose.getX() < 0 || estimation.estimatedPose.getX() > 16.2 || estimation.estimatedPose.getY() < 0 || estimation.estimatedPose.getY() > 7.7) {
+    // reject if pose thinks we're out of the field
+    if (estimation.estimatedPose.getX() < 0
+        || estimation.estimatedPose.getX() > 16.2
+        || estimation.estimatedPose.getY() < 0
+        || estimation.estimatedPose.getY() > 7.7) {
       return infiniteDevs;
     }
     // TAG_COUNT_DEVIATION_PARAMS
@@ -179,10 +182,17 @@ public class Camera {
               swerveEstimator.addVisionMeasurement(
                   visionPose.toPose2d(),
                   inputs.result.metadata.captureTimestampMicros / 1.0e6,
-                  //trust vision less in auto because the odo reset should be very accurate
-                  deviations.times(DriverStation.isAutonomous() ? 2.0 : 1.0)
-                  //trust vision more if odo tells us we're outside the field
-                  .times((swerveEstimator.getEstimatedPosition().getX() < 0 || swerveEstimator.getEstimatedPosition().getX() > 16.2 || swerveEstimator.getEstimatedPosition().getY() < 0 || swerveEstimator.getEstimatedPosition().getY() > 7.7) ? 0.25 : 1));
+                  // trust vision less in auto because the odo reset should be very accurate
+                  deviations
+                      .times(DriverStation.isAutonomous() ? 2.0 : 1.0)
+                      // trust vision more if odo tells us we're outside the field
+                      .times(
+                          (swerveEstimator.getEstimatedPosition().getX() < 0
+                                  || swerveEstimator.getEstimatedPosition().getX() > 16.2
+                                  || swerveEstimator.getEstimatedPosition().getY() < 0
+                                  || swerveEstimator.getEstimatedPosition().getY() > 7.7)
+                              ? 0.25
+                              : 1));
               // the sussifier
             });
 
